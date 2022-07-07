9adc5fd028ec402a2c4cfe31865f642c02c9eb3f
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-07-07 15:06:21 +0200

    Added syntax highlighting

--- client/ui/src/components/text-editor/TextEditor.js
+++ client/ui/src/components/text-editor/TextEditor.js
@@ -1,47 +1,49 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import "./TextEditor.css"
 import axios from 'axios'
+import CodeMirror from '@uiw/react-codemirror';
+import {dracula} from '@uiw/codemirror-theme-dracula';
+import {cpp} from '@codemirror/lang-cpp';
 
 
 var updatedFile = ""
 var path = ""
 
-const TextEditor = (props) =>
-{
+const TextEditor = (props) => {
 
-    const [fileName, setFileName] = useState("default") 
+    const [fileName, setFileName] = useState("default")
     const [fileContent, setFileContent] = useState("")
 
-    const saveFile = () =>{
+    const saveFile = () => {
 
-        const getFileContent = async () =>{
-            try{
-            let headers = {
-                'Content-Type': 'text/plain'
-            }
-            const res = await axios.post('http://localhost:4567/file', "{'path' : '" + path + "', 'content' : '" + updatedFile + "'}" , {headers})
-            console.log(res.data)
+        const getFileContent = async () => {
+            try {
+                let headers = {
+                    'Content-Type': 'text/plain'
+                }
+                const res = await axios.post('http://localhost:4567/file', "{'path' : '" + path + "', 'content' : '" + fileContent + "'}", {headers})
+                console.log(res.data)
             } catch (e) {
                 console.log(e)
             }
         };
         getFileContent()
     }
-    const openFile = () =>{
+    const openFile = () => {
 
         //pop to enter path of the file
         let pathOfTheFileToOpen = prompt("Please enter the path of the file that you want to open")
         // get the file from api
         path = pathOfTheFileToOpen;
 
-        const getFileContent = async () =>{
-            try{
-            let headers = {
-                'Content-Type': 'text/plain'
-            }
-            const res = await axios.post('http://localhost:4567/info/file', "{'path' : '" + pathOfTheFileToOpen + "'}" , {headers})
-            setFileContent(res.data.data)
-            console.log(res.data.data)
+        const getFileContent = async () => {
+            try {
+                let headers = {
+                    'Content-Type': 'text/plain'
+                }
+                const res = await axios.post('http://localhost:4567/info/file', "{'path' : '" + pathOfTheFileToOpen + "'}", {headers})
+                setFileContent(res.data.data)
+                console.log(res.data.data)
             } catch (e) {
                 console.log(e)
             }
@@ -50,27 +52,29 @@ const TextEditor = (props) =>
         setFileName("test.c");
     }
 
-  const OpenFile = props.openFile
-  const DuplicateFile = props.DuplicateFile
-  const closeFile = props.closeFile
+    const OpenFile = props.openFile
+    const DuplicateFile = props.DuplicateFile
+    const closeFile = props.closeFile
 
-    return(
-<>
-    <div class="topnav">
-  <a class="active">{fileName}</a> 
-  <button className='btn' onClick={closeFile}>{!props.data ? "Akaiky": "Close"}</button>
-  <button className='btn' onClick={DuplicateFile}>{!props.data ? "Saraho": "Split"}</button>
-  <button className='btn' onClick={openFile}>{!props.data ? "Misokatra": "Open"}</button>
-  <button className='btn' onClick={saveFile}>{!props.data ? "Afa-tsy": "Save"}</button>
-</div>
-    <div className="editor" contentEditable="true" onInput={(e) => {
-    updatedFile = e.currentTarget.textContent
-    console.log(updatedFile)}}
-    >
-      {fileContent}
-    </div>
-</>
-) 
+    return (
+        <>
+            <div class="topnav">
+                <a class="active">{fileName}</a>
+                <button className='btn' onClick={closeFile}>{!props.data ? "Akaiky" : "Close"}</button>
+                <button className='btn' onClick={DuplicateFile}>{!props.data ? "Saraho" : "Split"}</button>
+                <button className='btn' onClick={openFile}>{!props.data ? "Misokatra" : "Open"}</button>
+                <button className='btn' onClick={saveFile}>{!props.data ? "Afa-tsy" : "Save"}</button>
+            </div>
+            <CodeMirror
+                value={fileContent}
+                theme={dracula}
+                height="100vh"
+                extensions={[cpp()]}
+                onChange={setFileContent}
+            />
+
+        </>
+    )
 }
 export default TextEditor;
 
